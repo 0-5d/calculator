0ad5517bec56e6c5e9bae6e7e1d4dee3479f1d4e
K <kaisontoro@gmail.com>
2021-12-31 22:34:33 -0700

    oh yeah

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
 const numpad = document.getElementById('numpad')
-let displayValue;
-function add(additionArray) {
-    let total = 0;
-    for (i = 0; i < additionArray.length; i++) {
-        total += additionArray[i];
-    }
-    return total;
-}
+let displayValue = '';
+let secondOperand;
+let operatorPressed;
+let currentOperator;
+let operandArray;
+let answer;
+let equalsPressed;
+let numberPressed;
 
-function subtract(subtractionArray) {
-    let total = subtractionArray[0];
-    for (i = 1; i < subtractionArray.length; i++) {
-        total -= subtractionArray[i];
-    }
-    return total;
-}
-
-function multiply(multiplicationArray) {
-    let total = 1;
-    for (i = 0; i< multiplicationArray.length; i++) {
-        total *= multiplicationArray[i];
-    }
-    return total;
-}
-
-function buttonide(buttonisionArray) {
-    let total = buttonisionArray[0];
-    for (i= 1; i < buttonisionArray.length; i++){
-        total /= buttonisionArray[i];
-    }
-    return total;
-}
-
-function operate(operator, x, y) {
-    return operator([x,y]);
-}
 
 function generateNumpad() {
     const row1 = document.getElementById('row1')
@@ -42,18 +15,35 @@ function generateNumpad() {
     const row3 = document.getElementById('row3')
     const row4 = document.getElementById('row4')
     for (i = 0; i < 9; i++) {
-        let poopoo = i;
+        let iPlaceHolder = i;
         switch(true){
             case i < 3:
                 const newKey = document.createElement('button');
                 newKey.textContent = i+1;
                 newKey.classList.add('bigKey')
-                newKey.addEventListener("click", displayNumber(i + 1))
+                newKey.addEventListener("click", (i) => {
+                    if (!equalsPressed) {
+                        const display = document.getElementById('display')
+                        display.textContent += iPlaceHolder + 1
+                        displayValue += iPlaceHolder + 1
+                        numberPressed = true;
+                    }
+                })
                 row1.appendChild(newKey);
                 if (i == 2) {
                     const plusSign = document.createElement('button');
                     plusSign.textContent = '+';
                     plusSign.classList.add('normalKey')
+                    plusSign.addEventListener("click", () => {
+                        if (numberPressed) {
+                            displayValue += '+';
+                            display.textContent = displayValue; 
+                            currentOperator = '+'
+                            operatorPressed = true;
+                            equalsPressed = false;
+                        }
+
+                    })
                     row1.appendChild(plusSign)
                 }
                 break;
@@ -61,12 +51,38 @@ function generateNumpad() {
                 const newKey2 = document.createElement('button');
                 newKey2.textContent = i+1;
                 newKey2.classList.add('bigKey')
-                newKey2.addEventListener("click", displayNumber(i + 1))
+                newKey2.addEventListener("click", (i) => {
+                    if (!equalsPressed) {
+                        const display = document.getElementById('display')
+                        display.textContent += iPlaceHolder + 1
+                        displayValue += iPlaceHolder + 1
+                        numberPressed = true;
+                    }
+                })
                 row2.appendChild(newKey2);
                 if (i == 5) {
                     const minusSign = document.createElement('button');
                     minusSign.textContent = '-';
                     minusSign.classList.add('normalKey')
+                    minusSign.addEventListener("click", () => {
+                        if (!operatorPressed && numberPressed) {
+                            displayValue += '-';
+                            display.textContent = displayValue; 
+                            currentOperator = '-'
+                            equalsPressed = false
+                            operatorPressed = true;
+                        } else if (operatorPressed) {
+                            operandArray = displayValue.split("-")
+                            answer = +operandArray[0] - +operandArray[1].toString()
+                            display.textContent = answer + '-';
+                            displayValue = answer + '-';
+
+                            operatorPressed = false
+                            currentOperator = ''
+                        }
+                        
+
+                    })
                     row2.appendChild(minusSign)
                 }
                 break;
@@ -75,8 +91,12 @@ function generateNumpad() {
                 newKey3.textContent = i+1;
                 newKey3.classList.add('bigKey')
                 newKey3.addEventListener("click", (i) => {
-                    const display = document.getElementById('display')
-                    display.textContent = 'ohyeah' + i
+                    if (!equalsPressed) {
+                        const display = document.getElementById('display')
+                        display.textContent += iPlaceHolder + 1
+                        displayValue += iPlaceHolder + 1
+                        numberPressed = true;
+                    }
                 })
                 row3.appendChild(newKey3);
                 break;
@@ -85,34 +105,106 @@ function generateNumpad() {
     const multiplicationSign = document.createElement('button')
     multiplicationSign.textContent = 'X'
     multiplicationSign.classList.add('normalKey')
+    multiplicationSign.addEventListener("click", () => {
+        if (!operatorPressed && numberPressed) {
+            displayValue += '*';
+            display.textContent = displayValue; 
+            currentOperator = '*'
+            equalsPressed = false;
+        }
+        operatorPressed = true;
+    })
     row3.appendChild(multiplicationSign)
 
-    const zeroKey = document.createElement('button')
+    const zeroKey = document.createElement('button');
     zeroKey.textContent = '0'
     zeroKey.classList.add('bigKey')
-    zeroKey.addEventListener("click", displayNumber(0))
+    zeroKey.addEventListener("click", () => {
+        const display = document.getElementById('display')
+        display.textContent += 0
+        displayValue += 0;
+    })
     row4.appendChild(zeroKey)
 
     const equalsKey = document.createElement('button')
     equalsKey.textContent = '='
     equalsKey.classList.add('bigKey')
+    equalsKey.addEventListener('click', () => {
+        switch(true) {
+            case currentOperator == "+":
+                operandArray = displayValue.split("+")
+                answer = +operandArray[0] + +operandArray[1].toString()
+                display.textContent = answer;
+                displayValue = answer;
+                equalsPressed = true;
+                operatorPressed = false
+                currentOperator = ''
+                break;
+            case currentOperator == "*":
+                operandArray = displayValue.split("*")
+                answer = +operandArray[0] * +operandArray[1].toString()
+                display.textContent = answer;
+                displayValue = answer;
+                equalsPressed = true;
+                operatorPressed = false
+                currentOperator = ''
+                break;
+            case currentOperator == "-":
+                operandArray = displayValue.split("-")
+                answer = +operandArray[0] - +operandArray[1].toString()
+                display.textContent = answer;
+                displayValue = answer;
+                equalsPressed = true;
+                operatorPressed = false
+                currentOperator = ''
+                break;
+            case currentOperator == "/":
+                operandArray = displayValue.split("/")
+                if (operandArray[1] == 0) {
+                    display.textContent = "no"
+                    break;
+                }
+                answer = +operandArray[0] / +operandArray[1].toString()
+                display.textContent = answer;
+                displayValue = answer;
+                equalsPressed = true;
+                operatorPressed = false
+                currentOperator = ''
+                break;
+        }
+    })
     row4.appendChild(equalsKey)
 
     const clearKey = document.createElement('button')
     clearKey.textContent = 'CLEAR'
     clearKey.classList.add('bigKey')
+    clearKey.addEventListener('click', () => {
+        const display = document.getElementById('display')
+        display.textContent = ''
+        displayValue = ''
+        answer = ''
+        currentOperator = ''
+        operandArray = []
+        operatorPressed = false;
+        equalsPressed = false;
+        numberPressed = false;
+    })
     row4.appendChild(clearKey)
 
-    const divisionKey = document.createElement('button')
-    divisionKey.textContent = '/'
-    divisionKey.classList.add('normalKey')
-    row4.appendChild(divisionKey)
+    const divisionSign = document.createElement('button')
+    divisionSign.textContent = '/'
+    divisionSign.classList.add('normalKey')
+    divisionSign.addEventListener("click", () => {
+        if (!operatorPressed && numberPressed) {
+            displayValue += '/';
+            display.textContent = displayValue; 
+            currentOperator = '/'
+            equalsPressed = false;
+        }
+        operatorPressed = true;
+    })
+    row4.appendChild(divisionSign)
 
-}
-
-function displayNumber(buttonPressed) {
-    const display = document.getElementById('display');
-    display.textContent += buttonPressed;  
 }
 
 generateNumpad()
